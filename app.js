23df8fad6f406a82ea8a554a1edbd060055214e2
aalmalood <almalood.91@gmail.com>
2024-01-15 19:30:53 +0300

    new button

--- app.js
+++ app.js
@@ -187,6 +187,46 @@
 		pla.blueTeam = shuffle(pla.blueTeam);
 		pla.redTeam = shuffle(pla.redTeam);
 		}
+		pla.oldGanerate = function oldGanerate(){
+			console.log("before shuffle " , pla.matchPlayer);
+			pla.matchPlayer = shuffle(pla.matchPlayer);
+			console.log("after shuffle " , pla.matchPlayer);
+			var result = oldEqualTeams(pla.matchPlayer);
+		
+		pla.blueTeam = result.teams[0];
+		pla.redTeam = result.teams[1];
+		pla.blueoldTeamStrength = result.strengths[0];
+		pla.redoldTeamStrength = result.strengths[1];
+		if(pla.matchPlayer.length % 2 == 1){
+			var weekerTeam = 1 ;
+			if(pla.blueoldTeamStrength < pla.redoldTeamStrength){
+				weekerTeam = 0;
+			}
+			var leftedPlayer = pla.matchPlayer.slice();
+			for(var i = 0 ; i < pla.blueTeam.length ; i ++){
+				var player1 = pla.blueTeam[i];
+				var player2 = pla.redTeam[i];
+				var idx = leftedPlayer.indexOf(player1);
+				if (idx > -1) {
+					leftedPlayer.splice(idx, 1);
+				}
+				var idx = leftedPlayer.indexOf(player2);
+				if (idx > -1) {
+					leftedPlayer.splice(idx, 1);
+				}
+			}
+			if(leftedPlayer != null && leftedPlayer.length > 0){
+				if(weekerTeam == 0){
+					pla.blueTeam.push(leftedPlayer[0]);
+					pla.blueoldTeamStrength = pla.blueoldTeamStrength + leftedPlayer[0].strength;
+				}else{
+					pla.redTeam.push(leftedPlayer[0]);
+					pla.redoldTeamStrength = pla.redoldTeamStrength + leftedPlayer[0].strength;
+				}
+				
+			}
+		}
+		}
 		
 		
 
@@ -308,6 +348,95 @@
 			swapPlayers(wTeam, sTeam, wBestSel, sBestSel);
 			return {teams: [wTeam, sTeam], strengths: [teamStrength(wTeam), teamStrength(sTeam)]};
 		}
+		function oldCompareStrength(a, b) { // for sorting players and selections
+			return a.strength - b.strength;
+		}
+		function oldTeamStrength(players) {
+			return players.reduce(function(total, player) {return total + player.strength;}, 0);
+		}
+		function oldSelectionStrength(players, selection) {
+			return players.reduce(function(total, player, index) {return total + player.strength * selection[index];}, 0);
+		}
+		function oldNextPermutation(selection) { // reverse-lexicographical next permutation of a bit array
+			var max = true, pos = selection.length, set = 1;
+			while (pos-- && (max || !selection[pos])) if (selection[pos]) ++set; else max = false;
+			if (pos < 0) return false;
+			selection[pos] = 0;
+			while (++pos < selection.length) selection[pos] = set-- > 0 ? 1 : 0;
+			return true;
+		}
+		function oldSwapPlayers(wTeam, sTeam, wSelect, sSelect) {
+			for (var i = 0, j = 0; i < wSelect.length; i++) {
+				if (wSelect[i]) {
+					while (!sSelect[j]) ++j;
+					var temp = wTeam[i];
+					wTeam[i] = sTeam[j];
+					sTeam[j++] = temp;
+				}
+			}
+		}
+
+		function oldEqualTeams(players) {
+			// SORT PLAYERS FROM WEAKEST TO STRONGEST
+			players.sort(oldCompareStrength);
+			// INITIAL DISTRIBUTION OF PLAYERS INTO WEAKER AND STRONGER TEAM (ALTERNATING)
+			var wTeam = [], sTeam = [];
+			for (var i = players.length % 2; i < players.length; i += 2) {
+				wTeam.push(players[i]);
+				sTeam.push(players[i + 1]);
+			}
+			/*if(players.length % 2 ==1){
+				sTeam.push(players[players.length -1]);
+			}*/
+			var teamSize = wTeam.length;
+			// CALCULATE INITIAL STRENGTH DIFFERENCE
+			var initDiff = oldTeamStrength(sTeam) - oldTeamStrength(wTeam);
+			var bestDiff = initDiff;
+			var wBestSel = [], sBestSel = [];
+			// CHECK SELECTIONS OF EVERY SIZE
+			for (var selSize = 1; selSize < teamSize && bestDiff > 1; selSize++) {
+				var wSelections = [], sSelections = [], selection = [];
+				// CREATE INITIAL SELECTION BIT-ARRAY FOR WEAKER TEAM (SKIP PLAYER 1)
+				for (var i = 0; i < teamSize; i++)
+					selection[i] = (i > 0 && i <= selSize) ? 1 : 0;
+				// STORE ALL SELECTIONS FROM WEAKER TEAM AND THEIR STRENGTH
+				do wSelections.push({selection: selection.slice(), strength: oldSelectionStrength(wTeam, selection)});
+				while (oldNextPermutation(selection));
+				// SORT SELECTIONS FROM WEAKEST TO STRONGEST
+				wSelections.sort(oldCompareStrength);
+				// CREATE INITIAL SELECTION BIT-ARRAY FOR STRONGER TEAM
+				for (var i = 0; i < teamSize; i++)
+					selection[i] = (i < selSize) ? 1 : 0;
+				// STORE ALL SELECTIONS FROM STRONGER TEAM AND THEIR STRENGTH
+				do sSelections.push({selection: selection.slice(), strength: oldSelectionStrength(sTeam, selection)});
+				while (oldNextPermutation(selection));
+				// SORT SELECTIONS FROM WEAKEST TO STRONGEST
+				sSelections.sort(oldCompareStrength);
+				// ITERATE OVER SELECTIONS FROM BOTH TEAMS
+				var wPos = 0, sPos = 0;
+				while (wPos < wSelections.length && sPos < sSelections.length) {
+					// CALCULATE STRENGTH DIFFERENCE IF THESE SELECTIONS WERE SWAPPED
+					var wStrength = wSelections[wPos].strength, sStrength = sSelections[sPos].strength;
+					var diff = Math.abs(initDiff - 2 * (sStrength - wStrength));
+					// SET NEW BEST STRENGTH DIFFERENCE IF SMALLER THAN CURRENT BEST
+					if (diff < bestDiff) {
+						bestDiff = diff;
+						wBestSel = wSelections[wPos].selection.slice();
+						sBestSel = sSelections[sPos].selection.slice();
+						// STOP SEARCHING IF PERFECT SOLUTION FOUND (DIFFERENCE 0 OR 1)
+						if (bestDiff < 2) break;
+					}
+					// ADVANCE TO NEXT SELECTION FROM WEAKER OR STRONGER TEAM
+					if (2 * (sStrength - wStrength) > initDiff) ++wPos; else ++sPos;
+				}
+			}
+			// PERFORM SWAP OF BEST PAIR OF SELECTIONS FROM EACH TEAM
+			oldSwapPlayers(wTeam, sTeam, wBestSel, sBestSel);
+			return {teams: [wTeam, sTeam], strengths: [oldTeamStrength(wTeam), oldTeamStrength(sTeam)]};
+		}
+		
+		
+	  
 		
 		
 	  }
